4ada4d72efc14d5cd4eb5dd73c7bc5198fa1eee7
wangxuchu <wangxuchu704@gmail.com>
2020-02-05 19:36:27 +0800

    my first helloworld program

---
+++ javaseproject/src/com/wxc/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.wxc.java;
+
+/**
+ * @Classname HelloWorld
+ * @Description TODO
+ * @Date 2020/2/5 18:05
+ * @Created by allen
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
